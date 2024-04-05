6e30c56a756c80d890b6c53c3f96e1393c2cc415
DeewhyCodes <131259901+DeewhyCodes@users.noreply.github.com>
2024-04-05 15:52:02 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'https://54.174.199.204/:9000/',
+  serverUrl: 'http://54.174.199.204/:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '5744874518ee4e374e62b0286dc58f685a4fb3ee',
+	    'sonar.login': '552942fb5ca2514b60638d13e2a248510987eec1',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
