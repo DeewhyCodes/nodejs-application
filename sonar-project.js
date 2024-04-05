af4ac62138360ab1beaad781bd1d33f728c3d8bb
DeewhyCodes <131259901+DeewhyCodes@users.noreply.github.com>
2024-04-05 15:50:09 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.174.199.204/:9000/',
+  serverUrl: 'https://54.174.199.204/:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
